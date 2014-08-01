e0b65355a55cf5dd942faa5508cd96be7a8469e7
Anton Yu <antonyu@uchicago.edu>
2014-08-01 10:53:45 -0500

    removed fax-end.sh

--- fax-end.sh
+++
@@ -1,4 +0,0 @@
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
-export RUCIO_ACCOUNT=jenkins
-localSetupFAX --rootVersion=current-SL6
